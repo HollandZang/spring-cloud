3e0865784a9185b793e717145f9a0189300b2c93
holland <514503337@qq.com>
2021-12-03 15:11:54 +0800

    2021年12月3日

--- common/src/main/java/com/holland/common/entity/gateway/User.java
+++ common/src/main/java/com/holland/common/entity/gateway/User.java
@@ -1,35 +1,45 @@
 package com.holland.common.entity.gateway;
 
-import java.io.Serializable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 /**
  * 用户信息表
+ *
  * @TableName user
  */
-public class User implements Serializable {
+public class User implements UserDetails {
+    private LocalDateTime loginTime = LocalDateTime.now();
+
     /**
-     * 
+     *
      */
     private Integer id;
 
     /**
-     * 
+     *
      */
     private String loginName;
 
     /**
-     * 
+     *
      */
     private String password;
 
     /**
-     * 
+     *
      */
     private Date createTime;
 
     /**
-     * 
+     *
      */
     private Date updateTime;
 
@@ -53,8 +63,41 @@ public class User implements Serializable {
         return this;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("admin"));
+        return authorities;
+    }
+
+    @Override
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return loginName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return loginTime.plusMinutes(30).isBefore(LocalDateTime.now());
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public User setPassword(String password) {
@@ -80,7 +123,4 @@ public class User implements Serializable {
         return this;
     }
 
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
 }
