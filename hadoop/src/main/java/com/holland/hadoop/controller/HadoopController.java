f1f9ac8f821d6c9a69c8869c6c47fee432581d4f
haonan.zhang <haonan.zhang1234@Yx>
2022-05-13 16:58:57 +0800

    update

--- hadoop/src/main/java/com/holland/hadoop/controller/HadoopController.java
+++ hadoop/src/main/java/com/holland/hadoop/controller/HadoopController.java
@@ -2,9 +2,7 @@ package com.holland.hadoop.controller;
 
 import com.holland.common.utils.Response;
 import com.holland.hadoop.conf.HadoopConfig;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.io.InputStream;
@@ -15,20 +13,20 @@ public class HadoopController {
     @Resource
     private HadoopConfig hadoop;
 
-    @PostMapping("/createDirectory")
-    public <T> Response<T> createDirectory(String dir) {
+    @PostMapping("/directory/{dir}")
+    public <T> Response<T> createDirectory(@PathVariable String dir) {
         hadoop.createDirectory(dir);
         return Response.success();
     }
 
-    @PostMapping("/writeFile")
-    public <T> Response<T> writeFile(String path, String fileName, InputStream inputStream) {
+    @PostMapping("/file/{path}/{fileName}")
+    public <T> Response<T> writeFile(@PathVariable String path, @PathVariable String fileName, InputStream inputStream) {
         hadoop.writeFile(path, fileName, inputStream);
         return Response.success();
     }
 
-    @PostMapping("/downloadFile")
-    public Response<byte[]> downloadFile(String path, String fileName) {
+    @GetMapping("/file/{path}/{fileName}")
+    public Response<byte[]> downloadFile(@PathVariable String path, @PathVariable String fileName) {
         final byte[] bytes = hadoop.downloadFile(path, fileName);
         return Response.success(bytes);
     }
