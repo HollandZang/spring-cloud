e91f4d5775a9a6f288664befe2cf48b93a63db14
ZhangHN <zhn.pop@gmail.com>
2023-01-10 10:19:21 +0800

    update

--- admin/src/main/java/com/holland/admin/AdminApplication.java
+++ admin/src/main/java/com/holland/admin/AdminApplication.java
@@ -1,13 +1,19 @@
 package com.holland.admin;
 
 
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.NacosServiceManager;
+import com.alibaba.cloud.nacos.discovery.NacosServiceDiscovery;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableAutoConfiguration
@@ -18,12 +24,17 @@ public class AdminApplication {
         SpringApplication.run(AdminApplication.class, args);
     }
 
-    @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                    .and().csrf().disable();
-        }
+//        @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange().anyExchange().permitAll()
+                .and().csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+    @Bean
+    public NacosServiceDiscovery nacosServiceDiscovery(
+            NacosDiscoveryProperties discoveryProperties,
+            NacosServiceManager nacosServiceManager) {
+        return new MyNacosServiceDiscovery(discoveryProperties, nacosServiceManager);
     }
 }
---
+++ admin/src/main/java/com/holland/admin/MyNacosServiceDiscovery.java
@@ -0,0 +1,53 @@
+package com.holland.admin;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.NacosServiceInstance;
+import com.alibaba.cloud.nacos.NacosServiceManager;
+import com.alibaba.cloud.nacos.discovery.NacosServiceDiscovery;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+public class MyNacosServiceDiscovery extends NacosServiceDiscovery {
+
+    private final NacosDiscoveryProperties discoveryProperties;
+    private final NacosServiceManager nacosServiceManager;
+
+    public MyNacosServiceDiscovery(NacosDiscoveryProperties discoveryProperties, NacosServiceManager nacosServiceManager) {
+        super(discoveryProperties, nacosServiceManager);
+        this.discoveryProperties = discoveryProperties;
+        this.nacosServiceManager = nacosServiceManager;
+    }
+
+    @Override
+    public List<ServiceInstance> getInstances(String serviceId) throws NacosException {
+        String group = discoveryProperties.getGroup();
+        List<Instance> instances = namingService().selectInstances(serviceId, group,
+                true);
+        List<ServiceInstance> serviceInstances = hostToServiceInstanceList(instances, serviceId);
+
+        // 通过元数据，把 内网IP 替换成 外网IP(visitHost)
+        {
+            serviceInstances.forEach(serviceInstance -> {
+                Map<String, String> metadata = serviceInstance.getMetadata();
+                String visitUrl = metadata.get("visitHost");
+                if (StringUtils.hasText(visitUrl)) {
+                    metadata.put("host", serviceInstance.getHost());
+                    ((NacosServiceInstance) serviceInstance).setHost(visitUrl);
+                }
+            });
+        }
+
+        return serviceInstances;
+    }
+
+    private NamingService namingService() {
+        return nacosServiceManager
+                .getNamingService(discoveryProperties.getNacosProperties());
+    }
+}
